81b9a31eac0627ebb0c17f9e321dd865906b47cb
Milak9 <milos.milakovic98@gmail.com>
2021-11-23 16:28:37 +0100

    [feat/ListAP]: Connect to server and get local WLAN

--- ListAP/ListAP.cpp
+++ ListAP/ListAP.cpp
@@ -3,8 +3,44 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    WlanWrapper ww;
-    ww.WlanOpenHandleToServer(0, NULL, 0, NULL);
+    DWORD maxClientVersion = 2;
+    DWORD curVersion = 0;
+    HANDLE clientHandle = NULL;
+
+    WlanWrapper wlanWrapper;
+
+    // 1. Povezivanje
+    DWORD openHandleResult = wlanWrapper.WlanOpenHandleToServer(maxClientVersion, NULL, &curVersion, &clientHandle);
+
+    if (openHandleResult != ERROR_SUCCESS)
+    {
+        std::cout << "WlanOpenHandleToServer failed with error: " << openHandleResult << std::endl;
+        return 1;
+    }
+    else
+    {
+        std::cout << "Connected to the server" << std::endl;
+    }
+
+    PWLAN_INTERFACE_INFO_LIST wlanInterfaceList = NULL;
+
+    // 2. Lista dostupnih WLAN interfejsa na lokalu
+    DWORD enumInterfacesResult = wlanWrapper.WlanEnumInterfacesWLAN(clientHandle, NULL, &wlanInterfaceList);
+    
+    if (enumInterfacesResult != ERROR_SUCCESS)
+    {
+        std::cout << "WlanEnumInterfacesWLAN failed with error: " << enumInterfacesResult << std::endl;
+        return 1;
+    }
+    else
+    {
+
+    }
+
+    if (wlanInterfaceList != NULL) {
+        wlanWrapper.WlanFreeAllocatedMemory(wlanInterfaceList);
+        wlanInterfaceList = NULL;
+    }
+
     return 0;
 }
