d72828c235a148ab2b12cdb17308b78a87adcdee
Milak9 <milos.milakovic98@gmail.com>
2021-11-24 14:11:18 +0100

    [feat/ListAP]: List all available access points

--- ListAP/ListAP.cpp
+++ ListAP/ListAP.cpp
@@ -3,6 +3,8 @@
 
 int main()
 {
+    int retVal = 0;
+
     DWORD maxClientVersion = 2;
     DWORD curVersion = 0;
     HANDLE clientHandle = NULL;
@@ -26,21 +28,75 @@ int main()
 
     // 2. Lista dostupnih WLAN interfejsa na lokalu
     DWORD enumInterfacesResult = wlanWrapper.WlanEnumInterfacesWLAN(clientHandle, NULL, &wlanInterfaceList);
-    
+
     if (enumInterfacesResult != ERROR_SUCCESS)
     {
         std::cout << "WlanEnumInterfacesWLAN failed with error: " << enumInterfacesResult << std::endl;
-        return 1;
+        retVal = 1;
     }
     else
     {
+        std::cout << "Num of interfaces: " << wlanInterfaceList->dwNumberOfItems << std::endl;
 
+        WCHAR guidOfTheInterfaceString[39] = { 0 };
+
+        PWLAN_INTERFACE_INFO interfaceInfo = wlanInterfaceList->InterfaceInfo;
+        int numOfCharacters = StringFromGUID2(interfaceInfo->InterfaceGuid, (LPOLESTR)&guidOfTheInterfaceString, sizeof(guidOfTheInterfaceString) / sizeof(*guidOfTheInterfaceString));
+
+        if (numOfCharacters == 0)
+        {
+            std::cout << "StringFromGUID2 failed" << std::endl;
+        }
+        else
+        {
+            std::cout << "Guid of the interface 1: " << guidOfTheInterfaceString << std::endl;
+        }
+
+        PWLAN_AVAILABLE_NETWORK_LIST netList = NULL;
+
+        // 3. Lista dostupnih mreza
+        DWORD availableNetworkResult = wlanWrapper.WlanGetAvailableNetworkListOnLAN(clientHandle, &interfaceInfo->InterfaceGuid, 0, NULL, &netList);
+
+        if (availableNetworkResult != ERROR_SUCCESS)
+        {
+            std::cout << "WlanGetAvailableNetworkListOnLAN failed with error: " << availableNetworkResult << std::endl;
+            retVal = 1;
+        }
+        else
+        {
+            std::cout << "Number of available networks: " << netList->dwNumberOfItems << std::endl;
+
+            PWLAN_AVAILABLE_NETWORK availableNet = NULL;
+
+            for (int i = 0; i < netList->dwNumberOfItems; i++)
+            {
+                availableNet = (WLAN_AVAILABLE_NETWORK*)&netList->Network[i];
+
+                if (availableNet->dot11Ssid.uSSIDLength == 0)
+                {
+                    std::cout << "This network doesn't have name" << std::endl;
+                }
+                else
+                {
+                    std::cout << availableNet->strProfileName << std::endl;
+                    std::cout << availableNet->dot11Ssid.ucSSID << std::endl;
+                }
+            }
+        }
+
+
+        if (netList != NULL)
+        {
+            wlanWrapper.WlanFreeAllocatedMemory(netList);
+            netList = NULL;
+        }
     }
+    
 
     if (wlanInterfaceList != NULL) {
         wlanWrapper.WlanFreeAllocatedMemory(wlanInterfaceList);
         wlanInterfaceList = NULL;
     }
 
-    return 0;
+    return retVal;
 }
